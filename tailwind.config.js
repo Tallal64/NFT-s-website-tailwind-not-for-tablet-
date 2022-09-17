2b6d9d0972f8b653cdce913f85e8271118fa31cb
Tallal <108980921+Tallal64@users.noreply.github.com>
2022-09-17 21:26:55 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        darkblue: '#041148',
+        pink: '#8E8EDB',
+        lightgreen: '#94CFCF',
+        lightpink: '#7D7ED6',
+        navyblue: '#28225D',
+        greenishyellow: '#AAD9D9',
+        footerbg: '#211F59',
+        submit: '#8080D7',
+      },
+    },
+  },
+  plugins: [],
+}
